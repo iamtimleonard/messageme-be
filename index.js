63ef8bbc01dfdc2d524dfd1a11d721eb18618a9f
iamtimleonard <timleonardcello@gmail.com>
2021-04-07 09:43:01 -0400

    add procfile

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const app = require("express")();
 const http = require("http").createServer(app);
+import cors from "cors";
 const io = require("socket.io")(http, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
 });
+
+app.use(cors());
 
 io.on("connection", (socket) => {
   socket.emit("new user", "welcome new user");
